65e712acaa6320c1d764b6eb8ae47de6931c26b4
Fintan Maguire <fintanmaguire@Fintan-Maguires-MacBook-Pro-NI249.local>
2019-09-03 15:33:59 +0100

    removing unused true statement

--- pg2kinesis/stream.py
+++ pg2kinesis/stream.py
@@ -41,7 +41,7 @@ class StreamWriter(object):
             self._send_agg_record(agg_record)
             self.last_send = time.time()
 
-        return agg_record or True
+        return agg_records
 
     def _send_agg_record(self, agg_record):
         if agg_record is None:
